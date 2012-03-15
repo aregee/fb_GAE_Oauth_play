7aab497dd15ff82d671f99169b2461852b9d7cd2
Rahul Gaur <rahul.nbg@gmail.com>
2012-03-16 05:08:28 +0530

    FB playing around app

---
+++ oauth/facebookoauth.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# last update - Aregee
+
+"""A barebones AppEngine application that uses Facebook for login.
+
+"""
+
+FACEBOOK_APP_ID = "219165848143592"
+FACEBOOK_APP_SECRET = "f752f57873d912c72035b84d222bdca9"
+
+import base64
+import cgi
+import Cookie
+import email.utils
+import hashlib
+import hmac
+import logging
+import os.path
+import time
+import urllib
+import wsgiref.handlers
+import os
+import urllib
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from django.utils import simplejson as json
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+
+
+class User(db.Model):
+    id = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    updated = db.DateTimeProperty(auto_now=True)
+    name = db.StringProperty(required=True)
+    profile_url = db.StringProperty(required=True)
+    access_token = db.StringProperty(required=True)
+
+
+class BaseHandler(webapp.RequestHandler):
+    @property
+    def current_user(self):
+        """Returns the logged in Facebook user, or None if unconnected."""
+        if not hasattr(self, "_current_user"):
+            self._current_user = None
+            user_id = parse_cookie(self.request.cookies.get("fb_user"))
+            if user_id:
+                self._current_user = User.get_by_key_name(user_id)
+        return self._current_user
+
+
+class HomeHandler(BaseHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), "oauth.html")
+        args = dict(current_user=self.current_user)
+        self.response.out.write(template.render(path, args))
+	
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        verification_code = self.request.get("code")
+        args = dict(client_id=FACEBOOK_APP_ID, redirect_uri=self.request.path_url)
+        if self.request.get("code"):
+            args["client_secret"] = FACEBOOK_APP_SECRET
+            args["code"] = self.request.get("code")
+            response = cgi.parse_qs(urllib.urlopen(
+                "https://graph.facebook.com/oauth/access_token?" +
+                urllib.urlencode(args)).read())
+            access_token = response["access_token"][-1]
+
+            # Download the user profile and cache a local instance of the
+            # basic profile info
+            profile = json.load(urllib.urlopen(
+                "https://graph.facebook.com/me?" +
+                urllib.urlencode(dict(access_token=access_token))))
+            user = User(key_name=str(profile["id"]), id=str(profile["id"]),
+                        name=profile["name"], access_token=access_token,
+                        profile_url=profile["link"])
+            user.put()
+            set_cookie(self.response, "fb_user", str(profile["id"]),
+                       expires=time.time() + 30 * 86400)
+            self.redirect("/")
+	    self.redirect('/main')
+        else:
+            self.redirect(
+                "https://graph.facebook.com/oauth/authorize?" +
+                urllib.urlencode(args))
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        set_cookie(self.response, "fb_user", "", expires=time.time() - 86400)
+        self.redirect("/")
+
+
+def set_cookie(response, name, value, domain=None, path="/", expires=None):
+    """Generates and signs a cookie for the give name/value"""
+    timestamp = str(int(time.time()))
+    value = base64.b64encode(value)
+    signature = cookie_signature(value, timestamp)
+    cookie = Cookie.BaseCookie()
+    cookie[name] = "|".join([value, timestamp, signature])
+    cookie[name]["path"] = path
+    if domain: cookie[name]["domain"] = domain
+    if expires:
+        cookie[name]["expires"] = email.utils.formatdate(
+            expires, localtime=False, usegmt=True)
+    response.headers._headers.append(("Set-Cookie", cookie.output()[12:]))
+
+
+def parse_cookie(value):
+    """Parses and verifies a cookie value from set_cookie"""
+    if not value: return None
+    parts = value.split("|")
+    if len(parts) != 3: return None
+    if cookie_signature(parts[0], parts[1]) != parts[2]:
+        logging.warning("Invalid cookie signature %r", value)
+        return None
+    timestamp = int(parts[1])
+    if timestamp < time.time() - 30 * 86400:
+        logging.warning("Expired cookie %r", value)
+        return None
+    try:
+        return base64.b64decode(parts[0]).strip()
+    except:
+        return None
+
+
+def cookie_signature(*parts):
+    """Generates a cookie signature.
+
+    We use the Facebook app secret since it is different for every app (so
+    people using this example don't accidentally all use the same secret).
+    """
+    hash = hmac.new(FACEBOOK_APP_SECRET, digestmod=hashlib.sha1)
+    for part in parts: hash.update(part)
+    return hash.hexdigest()
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+	upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write('<html><body><a href="/">Home</`a>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
+
+        for b in blobstore.BlobInfo.all():
+            self.response.out.write('<li><a href="/serve/%s' % str(b.key()) + '">' + str(b.filename) + '</a>')
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file')
+        blob_info = upload_files[0]
+        self.redirect('/main')
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, blob_key):
+        blob_key = str(urllib.unquote(blob_key))
+        if not blobstore.get(blob_key):
+            self.error(404)
+        else:
+            self.send_blob(blobstore.BlobInfo.get(blob_key), save_as=True)
+
+def main():
+    util.run_wsgi_app(webapp.WSGIApplication([
+        (r"/", HomeHandler),
+        (r"/auth/login", LoginHandler),
+        (r"/auth/logout", LogoutHandler),
+	(r"/main", MainHandler),
+	(r"/upload", UploadHandler),
+	(r"/serve/([^/]+)?", ServeHandler),
+    ],debug=True))
+
+
+if __name__ == "__main__":
+    main()
